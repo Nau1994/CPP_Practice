d79ea079d03a911d6ffe237faf16a4fda8b01cfe
Naushad <naushad.a1013@gmail.com>
2022-06-01 09:24:56 +0530

    findSubset

---
+++ findSubset.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+// #include <iostream>
+// #include <vector>
+// #include <string>
+
+using namespace std;
+void findSubset(char *input,char *output,int i,int j){
+//base codition
+if (input[i]=='\0'){
+
+ 	output[j]='\0';
+ 	if (output[0]=='\0') {cout<<"NULL";}
+	cout<<output<<endl; 
+	return;
+}
+
+//rec condition	
+output[j]=input[i];
+findSubset(input,output,i+1,j+1);
+
+//output[j]='\0';
+findSubset(input,output,i+1,j);
+
+}
+
+int main()
+{
+    
+    char input[100],output[100];
+    cin>>input;
+    findSubset(input,output,0,0);
+    cout<<endl;
+    return 0;
+}

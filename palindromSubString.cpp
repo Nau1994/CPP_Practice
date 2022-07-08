b0b5617227bb13e1f1dfd47c450c1dd1f3392e60
Naushad <naushad.a1013@gmail.com>
2022-07-08 20:06:05 +0530

    subSetBitmask

--- palindromSubString.cpp
+++ palindromSubString.cpp
@@ -9,15 +9,6 @@ public:
 	vector<int> memo1;
 	vector<vector<int>> memo3;
 
-	// memoising?
-	// O(n*n)
-	bool is_palindrome(int i, int j) {
-		if (i >= j) return true;
-		// if (i == j - 1) return (s[i] == s[j]);
-		if (memo[i][j] != -1) return memo[i][j];
-		return memo[i][j] = is_palindrome(i + 1, j - 1) && (s[i] == s[j]);
-	}
-
 	void prints(int i,int j){
 		for(int k=i;k<=j;k++){
 			cout<<s[k];
@@ -26,30 +17,23 @@ public:
 
 	}
 
-	// states O(n)
+	// memoising?
+	// O(n*n)
+	bool is_palindrome(int i, int j) {
+		if (i >= j) return true;
+		// if (i == j - 1) return (s[i] == s[j]);
+		if (memo[i][j] != -1) return memo[i][j];
+		return memo[i][j] = is_palindrome(i + 1, j - 1) && (s[i] == s[j]);
+	}
+// states O(n)
 	int dp(int i) {
-		
 		if (i == s.size()) return 0;
-		 // if(i==j) return 1;
-
-		// if (memo3[i][j] != -1) return memo3[i][j];
-
 		int ans = 0;
-		// int ans = is_palindrome(i, j);
-		
-		
 		// O(n)
 		for (int k = i; k <= s.size()-1; k++) {
-			
-			// ans += dp(i, k)+dp(k + 1,j);
-
 			ans += is_palindrome(i, k);
-
-			
 		}
 		ans+=dp(i + 1);
-		
-		// return  memo3[i][j]=ans;
 		return  ans;
 	}
 

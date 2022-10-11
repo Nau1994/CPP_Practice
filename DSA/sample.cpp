c38fac7c83ceddbe545742f8ceefb2c0d5bdb92b
Naushad <naushad.a1013@gmail.com>
2022-10-11 16:08:07 +0530

    sample

---
+++ DSA/sample.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+// #include <iostream>
+// #include <vector>
+// #include <string>
+
+using namespace std;
+#define int long long int
+
+int32_t main()
+{
+    
+    int n;
+    cin>>n;
+    cout<<n+1;
+    cout<<endl;
+}
